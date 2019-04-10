5448cbd5777f02e6b9f4a3a6f48c1e47636abab7
michael.samara <michael.samara@versasuite.com>
2019-04-10 16:32:00 -0500

    file-preview-in-UI

--- src/components/ImageUpload.jsx
+++ src/components/ImageUpload.jsx
@@ -1,63 +1,77 @@
 import React, {Component} from 'react';
-import {storage} from '../firebase';
+//import {storage} from '../firebase';
 
 
 class ImageUpload extends Component {
     constructor(props){
         super(props);
         this.state = {
-            image: null,
-            url: null
+            file: null,
+            // url: null,
+            // showImage: []
          }
         this.handleChange = this.handleChange.bind(this);
-        this.handleUpload = this.handleUpload.bind(this);
+        //this.handleUpload = this.handleUpload.bind(this);
     }
     handleChange = e => {
-        if (e.target.files[0]){
-            const image = e.target.files[0];
-            this.setState(() => ({image}));
-        }
+        this.setState({
+            file: URL.createObjectURL(e.target.files[0])
+          })
+        // if (e.target.files[0]){
+        //     const file = e.target.files[0];
+        //     this.setState(() => ({file}));
+                   
     }
-    handleUpload =()=>{
-        const {image}= this.state;
-       const uploadTask = storage.ref(`images/${image.name}`).put(image);
-        uploadTask.on('state_changed',
-        (snapshot)=>{
-            // progress function
-        },
-        (error) => {
-            // error function
-            console.log(error);
-        },
-         () => {
-             // complete function
-             storage.ref('images').child(image.name).getDownloadURL().then(url => {
-                console.log(url);
-                this.setState({url});
-             })
-         });
-    }
+    // handleUpload =()=>{
+    //     this.setState(state => ({
+
+
+    //     }))
+    //     const {image}= this.state;
+    //    const uploadTask = storage.ref(`images/${image.name}`).put(image);
+    //     uploadTask.on('state_changed',
+    //     (snapshot)=>{
+    //         // progress function
+    //     },
+    //     (error) => {
+    //         // error function
+    //         console.log(error);
+    //     },
+    //      () => {
+    //          // complete function
+    //          storage.ref('images').child(image.name).getDownloadURL().then(url => {
+    //             console.log(url);
+    //             this.setState({url});
+    //          })
+    //      });
+    // }
     render() {
         const style ={
             height:'100vh',
             display:'flex',
-            flexDirection: 'column',
+            flexDirection: 'row',
             alignItems: 'center',
             justifyContent: 'center'
         };
-        const fileAttached = this.state.url;
+       // const fileAttached = this.state.url;
         return (
             <div style={style}>
-                <input type="file" onChange={this.handleChange}/>
+            <input type="file" onChange={this.handleChange}/>
+            <img src={this.state.file}alt="File Uploaded" height="300"width="400"/>
+                {/* <input type="file" onChange={this.handleChange}/>
+
                 <button onClick={this.handleUpload}>Upload</button>
+               
+               
                 <br/>
                 {fileAttached ? (
                 <img src={this.state.url}
                     alt="File Uploaded" height="300"width="400" />
                     ): (
-                        <img src="" alt="No file uploaded"/>)}
+                        <img src="" alt="No file uploaded"/>)} */}
             </div>
          )
     }
 }
+
 export default ImageUpload;
--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/storage';
+
 // Initialize Firebase
 var config = {
     apiKey: "AIzaSyA53hOAq-K03XntomY1tuWiLSoJ3UJJ268",
